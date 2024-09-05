2992b2e869fa5f7723ee81d2697903c015b26fba
ahmedGhaly <ahm.say14@gmail.com>
2024-09-05 11:59:48 +0300

    Refactor the Contact page and its test code to cleaner

--- src/main/java/pages/ContactPage.java
+++ src/main/java/pages/ContactPage.java
@@ -2,13 +2,14 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class ContactPage extends Base{
+import java.time.Duration;
 
-    public ContactPage(WebDriver driver) {
-        super(driver);
-    }
+public class ContactPage extends Base {
 
+    // Locators for contact form elements
     private final By firstNameInputLocator = By.cssSelector("input[id=\"name\"]");
     private final By emailInputLocator = By.cssSelector("input[id=\"email\"]");
     private final By phoneInputLocator = By.cssSelector("input[id=\"phone\"]");
@@ -17,17 +18,25 @@ public class ContactPage extends Base{
     private final By submitBtnLocator = By.cssSelector("button[id=\"contactFormSubmit\"]");
     private final By contactFormSuccessMsgLocator = By.cssSelector("div[class=\"alert alert-success alert-dismissible fade show mt-3\"]");
 
-    public void fillContactFormWithValidData(String name,String email,String phone,String subject,String message){
-        setTextElement(driver.findElement(firstNameInputLocator),name);
-        setTextElement(driver.findElement(emailInputLocator),email);
-        setTextElement(driver.findElement(phoneInputLocator),phone);
-        setTextElement(driver.findElement(subjectInputLocator),subject);
-        setTextElement(driver.findElement(messageInputLocator),message);
+    private final WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    public ContactPage(WebDriver driver) {
+        super(driver);
+    }
+
+    // Method to fill the contact form with valid data and submit it
+    public void fillContactFormWithValidData(String name, String email, String phone, String subject, String message) {
+        setTextElement(driver.findElement(firstNameInputLocator), name);
+        setTextElement(driver.findElement(emailInputLocator), email);
+        setTextElement(driver.findElement(phoneInputLocator), phone);
+        setTextElement(driver.findElement(subjectInputLocator), subject);
+        setTextElement(driver.findElement(messageInputLocator), message);
         click(submitBtnLocator);
     }
 
-    public boolean IsSuccess(){
+    // Method to check if the success message is displayed after form submission
+    public boolean isSuccess() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(contactFormSuccessMsgLocator));
         return driver.findElement(contactFormSuccessMsgLocator).isDisplayed();
     }
-
 }
--- src/test/java/tests/ContactFormTest.java
+++ src/test/java/tests/ContactFormTest.java
@@ -5,30 +5,38 @@ import org.testng.annotations.Test;
 import pages.ContactPage;
 import pages.NavBar;
 
-public class ContactFormTest extends TestBase{
+public class ContactFormTest extends TestBase {
 
-    ContactPage contact;
-    NavBar nav;
-    Faker fake =new Faker();
-    String firstName = fake.name().firstName();
-    String email = fake.internet().safeEmailAddress();
-    String phone = String.valueOf(fake.number().numberBetween(1000000,99999999));
-    String subject = fake.lorem().paragraph(2);
-    String message = fake.lorem().paragraph(4);
+    private ContactPage contactPage;
+    private NavBar navBar;
+    private final Faker faker = new Faker();
+
+    // Fake data for contact form fields
+    private final String firstName = faker.name().firstName();
+    private final String email = faker.internet().safeEmailAddress();
+    private final String phone = String.valueOf(faker.number().numberBetween(1000000, 99999999));
+    private final String subject = faker.lorem().paragraph(2);
+    private final String message = faker.lorem().paragraph(4);
+
     @Test
-    public void ContactTest() throws InterruptedException {
+    public void testContactFormSubmission() throws InterruptedException {
         logger = extent.createTest("Verify User can Contact us successfully");
-        contact = new ContactPage(driver);
-        nav = new NavBar(driver);
 
-        nav.navigateToContactUsPage();
+        // Initialize page objects
+        contactPage = new ContactPage(driver);
+        navBar = new NavBar(driver);
 
-        contact.fillContactFormWithValidData(firstName,email,phone,subject,message);
-        if (contact.IsSuccess()){
-            reporter("pass","Your message has been sent successfully, We will contact you as soon as possible. Thank you!");
-        }else {
-            reporter("fail","User can not send his message !");
+        // Navigate to the Contact Us page
+        navBar.navigateToContactUsPage();
+
+        // Fill and submit the contact form
+        contactPage.fillContactFormWithValidData(firstName, email, phone, subject, message);
+
+        // Verify if the success message is displayed
+        if (contactPage.isSuccess()) {
+            reporter("pass", "Your message has been sent successfully. We will contact you as soon as possible. Thank you!");
+        } else {
+            reporter("fail", "User could not send their message!");
         }
-
     }
 }
